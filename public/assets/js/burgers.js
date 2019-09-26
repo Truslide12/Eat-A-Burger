4c3b26c0c5bf5ba6c47769292b2c1b933abd454b
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-09-26 16:45:50 -0700

    added validation to input for empty string

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -24,22 +24,27 @@ $(function() {
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
+    var testName = $("#ca").val().trim();
+    if (testName.lenght > 0) {
 
-    var newBurger = {
-      name: $("#ca").val().trim(),
-      devoured: false
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: false
+      };
+      
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+        );
+      } else {
+        console.log("Input cannot be empty")
       }
-    );
   });
 });
